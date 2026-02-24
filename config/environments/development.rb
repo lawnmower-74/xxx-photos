fb3c337e7fb9c3afb6ca9dea322183d8db365a8c
lawnmower-74 <tonapricot@gmail.com>
2026-02-24 16:55:35 +0900

    外部からのアクセスを許可

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # スマホや別PCからのIPアドレス指定アクセスを許可する
+  config.hosts.clear
 end
